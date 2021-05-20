efe98ab0549315aac76663a9e8fe8231f3108ced
nikolay_peremitin <peremitinns@mail.ru>
2021-05-20 20:45:08 +0300

    add totalQty in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <nav class="navbar navbar-light bg-light mb-4">
     <span class="navbar-brand mb-0 h1">Navbar</span>
-    <span class="navbar-brand mb-0 h1">Cart (0)</span>
+    <span class="navbar-brand mb-0 h1">Cart ({{ totalQty }})</span>
   </nav>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  
+  computed: {
+    ...mapGetters(['cart']),
+    totalQty() {
+      return this.cart.reduce((acc, item) => acc + item.qty, 0);
+    }
+  }
 }
 </script>
 <style scoped>
